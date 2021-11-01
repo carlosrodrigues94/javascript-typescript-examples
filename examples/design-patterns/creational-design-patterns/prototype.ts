5f73846e6ef8dc12fd034ae7398fb4ae448f59ff
carlosrodrigues94 <carlos.1994hrs@gmail.com>
2021-11-01 17:09:18 -0300

    feat: prototype pattern

---
+++ examples/design-patterns/creational-design-patterns/prototype.ts
@@ -0,0 +1,47 @@
+class ComponentWithBackReference {
+  public prototype: Prototype;
+
+  constructor(prototype: Prototype) {
+    this.prototype = prototype;
+  }
+}
+
+class Prototype {
+  public primitive!: number;
+  public component!: Date;
+  public circularReference!: ComponentWithBackReference;
+
+  public clone(): this {
+    const clone = Object.create(this);
+
+    clone.component = Object.create(this.component);
+
+    clone.circularReference = {
+      ...this.circularReference,
+      prototype: Object.create(this),
+    };
+
+    return clone;
+  }
+}
+
+const current = new Prototype();
+
+current.primitive = 245;
+current.component = new Date();
+
+current.circularReference = new ComponentWithBackReference(current);
+
+const clone = current.clone();
+
+const primitiveIsEqual = current.primitive === clone.primitive;
+const componentIsEqual = current.component === clone.component;
+
+const circularReferenceIsEqual =
+  current.circularReference === clone.circularReference;
+
+console.log({
+  primitiveIsEqual, // true
+  componentIsEqual, // false
+  circularReferenceIsEqual, // false
+});
